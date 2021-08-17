58f2ab746b391fde24f3c71c23f7aa6816d192c3
Anle101 <46081192+Anle101@users.noreply.github.com>
2021-08-17 14:05:54 -0400

    Feed work

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,8 @@ import Feed from './Feed';
 import Axios from 'axios';
 function Home() {
 
-    const [FeedItems, setFeedItems] = useState({});
+    const [FeedItems, setFeedItems] = useState([]);
+    const [Retrieval, setRetrieval] = useState(false);
 
     const {CurrentProfile} = useContext(GlobalContext);
     const {setCurrentProfile} = useContext(GlobalContext);
@@ -24,8 +25,12 @@ function Home() {
                 }
             }).then((response) => { //if successful
                 console.log(response.data[1]);
-                setFeedItems({imgPath:response.data[0].image_dir, caption: response.data[0].picture_caption});
-                
+                setFeedItems(response.data); 
+
+                console.log(FeedItems[0]);
+                console.log(FeedItems[1]);
+                //setFeedItems({imgPath:response.data[0].image_dir, caption: response.data[0].picture_caption});
+                setRetrieval(true);
             }).catch((error) => {
                 console.log(error);
            
@@ -35,6 +40,11 @@ function Home() {
       }, []);
     //Assumes user is already logged in
     
+    function feedDisplay() { 
+        return (
+            FeedItems.map ((feedItem) => ( <Feed feedItem = {{imgPath:feedItem.image_dir, caption: feedItem.picture_caption}} />))
+        );
+    }
       
     
     return (
@@ -42,9 +52,17 @@ function Home() {
             <div className="background">
                 <motion.div initial={{opacity:0}} animate = {{ opacity: 1}} exit = {{opacity:0}} className="hwall"></motion.div>
             </div> 
-
-
-            <Feed  feedItem = {FeedItems}/>
+            {Retrieval &&
+           
+           
+            <>
+                <feedDisplay />
+                <Feed feedItem = {{imgPath:FeedItems[0].image_dir, caption: FeedItems[0].picture_caption}} />
+                <Feed feedItem = {{imgPath:FeedItems[1].image_dir, caption: FeedItems[1].picture_caption}} />
+            
+            </>
+            }
+           
         </motion.div>
     )
 }
